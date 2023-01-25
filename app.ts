37975c77f24e9524506070d3c2e1f58998062e8b
oanskyy <oana.anghelachi@gmail.com>
2023-01-25 13:55:08 +0000

    basics: add enum types

--- app.ts
+++ app.ts
@@ -1,17 +1,27 @@
-
-
-
-const person: {
-    name: string, 
-    hobbies: string[],  //ARRAY types
-    role: [number, string], //TUPLE types > Array types
-} = {
-	name: "Hana",
-	hobbies: ["martial arts", "reading", "cooking"],
-    role: [2, 'author']
+// custom type: ENUM types
+enum Role {
+	ADMIN,
+	READ_ONLY,
+	AUTHOR
 }
 
-// Array types
+const person = {
+	name: "Hana",
+	hobbies: ["martial arts", "reading", "cooking"],
+	role: Role.ADMIN
+}
+
+// const person: {
+//     name: string,
+//     hobbies: string[],  //ARRAY types
+//     role: [number, string], //TUPLE types (fixed length array) > Array types
+// } = {
+// 	name: "Hana",
+// 	hobbies: ["martial arts", "reading", "cooking"],
+//     role: [2, 'author']
+// }
+
+/* Array types */
 let favoriteActivities: string[]
 favoriteActivities = ["sports", "dine out", "shopping", "learn"]
 
