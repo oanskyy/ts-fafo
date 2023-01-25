9eb03978ecc3286d371c8dfcf0e8b9e249165f5d
oanskyy <oana.anghelachi@gmail.com>
2023-01-25 13:00:02 +0000

    basics: add array types

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log('Your code goes here...');
+const person = {
+	name: "Hana",
+	hobbies: ["martial arts", "reading", "cooking"]
+}
+
+// Array types
+let favoriteActivities: string[]
+favoriteActivities = ["sports", "dine out", "shopping", "learn"]
+
+console.log(person.name)
+
+for (const hobby of person.hobbies) {
+	console.log(hobby.toUpperCase)
+	// console.log(hobby.map()) !!ERROR
+}
