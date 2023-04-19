ce5ad2a367a105b6cb986fe9ae798272b4df914a
oanskyy <oana.anghelachi@gmail.com>
2023-04-19 20:37:27 +0400

    feat: add a getter

--- src/app.ts
+++ src/app.ts
@@ -167,6 +167,16 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 	private project: Project
 
+	// using a getter
+	// getter - function 
+	get persons() { 
+		if (this.project.people === 1) { 
+			return '1 person'
+		}else { 
+			return `${this.project.people} persons`
+		} 
+	}
+
 	constructor(hostId: string, project: Project) {
 		super("single-project", hostId, false, project.id)
 		this.project = project
@@ -177,7 +187,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 	renderContent(): void {
 		this.element.querySelector("h2")!.textContent = this.project.title
 		this.element.querySelector("h3")!.textContent =
-			this.project.people.toString()
+			this.persons + ' assigned'
 		this.element.querySelector("p")!.textContent = this.project.description
 	}
 }
