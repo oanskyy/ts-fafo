c8fd1ad226672735ea1adccbd06422350c4ef38d
oanskyy <oana.anghelachi@gmail.com>
2023-04-19 20:54:00 +0400

    feat: create interface for drag&drop

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+// drag& drop interfaces 
+interface Draggable { 
+	dragStartHandler(event: DragEvent): void
+	dragEndHandler(event: DragEvent): void
+}
+
+interface DragTarget { 
+	dragOverHandler(event: DragEvent): void
+	dropHandler(event: DragEvent): void
+	dragLeaveHandler(event: DragEvent): void
+}
+
+
 enum ProjectStatus {
 	Active,
 	Finished
