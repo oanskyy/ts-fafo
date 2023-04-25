d3b9df51f2b969c3cb79af39497810cc9fe9f061
oanskyy <oana.anghelachi@gmail.com>
2023-04-25 11:35:28 +0400

    feat: organize folder components

---
+++ src/components/project-input.ts
@@ -0,0 +1,97 @@
+/// <reference path="base-component.ts" />
+
+namespace App { 
+    // Project Input Class
+	// restructure prj input to take advantage of inheritance and let the base class do a lot of the job
+	export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+		titleInputElement: HTMLInputElement
+		descriptionInputElement: HTMLInputElement
+		peopleInputElement: HTMLInputElement
+
+		constructor() {
+			super("project-input", "app", true, "user-input")
+			// this.templateElement = document.getElementById(
+			// 	"project-input"
+			// )! as HTMLTemplateElement
+			// this.hostElement = document.getElementById("app")! as HTMLDivElement
+
+			// const importedNode = document.importNode(this.templateElement.content, true)
+			// this.element = importedNode.firstElementChild as HTMLFormElement
+			// this.element.id = "user-input"
+
+			this.titleInputElement = this.element.querySelector(
+				"#title"
+			) as HTMLInputElement
+			this.descriptionInputElement = this.element.querySelector(
+				"#description"
+			) as HTMLInputElement
+			this.peopleInputElement = this.element.querySelector(
+				"#people"
+			) as HTMLInputElement
+
+			this.configure()
+			// this.attach()
+		}
+
+		// convention: public methods should be before private ones
+		configure() {
+			this.element.addEventListener("submit", this.submitHandler)
+		}
+
+		renderContent() {}
+
+		private gatherUserInput(): [string, string, number] | void {
+			const enteredTitle = this.titleInputElement.value
+			const enteredDescription = this.descriptionInputElement.value
+			const enteredPeople = this.peopleInputElement.value
+
+			const titleValidatable: Validatable = {
+				value: enteredTitle,
+				required: true
+			}
+			const descriptionValidatable: Validatable = {
+				value: enteredDescription,
+				required: true,
+				minLength: 5
+			}
+			const peopleValidatable: Validatable = {
+				value: +enteredPeople,
+				required: true,
+				min: 1,
+				max: 5
+			}
+
+			if (
+				!validate(titleValidatable) ||
+				!validate(descriptionValidatable) ||
+				!validate(peopleValidatable)
+			) {
+				alert("Invalid input, please try again!")
+				return
+			} else {
+				return [enteredTitle, enteredDescription, +enteredPeople]
+			}
+		}
+
+		private clearInputs() {
+			this.titleInputElement.value = ""
+			this.descriptionInputElement.value = ""
+			this.peopleInputElement.value = ""
+		}
+
+		@autobind
+		private submitHandler(event: Event) {
+			event.preventDefault()
+			const userInput = this.gatherUserInput()
+			if (Array.isArray(userInput)) {
+				const [title, desc, people] = userInput
+				projectState.addProject(title, desc, people)
+				this.clearInputs()
+			}
+		}
+
+		// private attach() {
+		// 	this.hostElement.insertAdjacentElement("afterbegin", this.element)
+		// }
+	}
+}
---
+++ src/components/project-item.ts
@@ -0,0 +1,48 @@
+/// <reference path="base-component.ts" />
+
+namespace App {
+	// ProjectItem Class
+	export class ProjectItem
+		extends Component<HTMLUListElement, HTMLLIElement>
+		implements Draggable
+	{
+		private project: Project
+
+		// using a getter
+		// getter - function
+		get persons() {
+			if (this.project.people === 1) {
+				return "1 person"
+			} else {
+				return `${this.project.people} persons`
+			}
+		}
+
+		constructor(hostId: string, project: Project) {
+			super("single-project", hostId, false, project.id)
+			this.project = project
+		}
+
+		@autobind
+		dragStartHandler(event: DragEvent) {
+			console.log(event)
+			event.dataTransfer!.setData("text/plain", this.project.id)
+			event.dataTransfer!.effectAllowed = "move"
+		}
+
+		dragEndHandler(_: DragEvent): void {
+			console.log("dragEnd")
+		}
+
+		configure(): void {
+			this.element.addEventListener("dragstart", this.dragStartHandler)
+			this.element.addEventListener("dragend", this.dragEndHandler)
+		}
+
+		renderContent(): void {
+			this.element.querySelector("h2")!.textContent = this.project.title
+			this.element.querySelector("h3")!.textContent = this.persons + " assigned"
+			this.element.querySelector("p")!.textContent = this.project.description
+		}
+	}
+}
